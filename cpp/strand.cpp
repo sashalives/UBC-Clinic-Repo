c3be9fda4820aac1cd93151c7c7d9a5747cc7764
sashalives <seheinen@gmail.com>
2014-12-10 08:01:20 -0800

    Began energy outputting
    implemented shell of a strand class

---
+++ cpp/strand.cpp
@@ -0,0 +1,9 @@
+/* structure information container implementation
+ *
+ * author: Sasha Heinen
+ * Dec 8 2014
+ */
+
+#include "strand.h"
+
+using namespace std;
---
+++ cpp/strand.h
@@ -0,0 +1,20 @@
+/* structure information container header
+ *
+ * author: Sasha Heinen
+ * Dec 8 2014
+ */
+
+#include <string>
+
+using namespace std;
+
+class Structure
+{
+public:
+
+	Structure(string );
+
+private:
+
+
+};
